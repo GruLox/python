0273e350e283ddf73220382e9fe55bebfef9dc8e
GruLox <grundmarcell@gmail.com>
2022-09-30 07:48:36 +0200

    initial commit

---
+++ 01 - Kiírás/Feladat 03/main.py
@@ -0,0 +1,7 @@
+dynasty: str = "Ming"
+start : int = 1368
+end : int = 1644
+distance : int = 7200
+greatWall: str = "Kínai Nagyfal"
+
+print(f"A {dynasty} dinasztia {start} és {end} közt építette a ma látható {greatWall} {distance}km hosszú szakaszát.")
---
+++ 01 - Kiírás/Feladat 04/main.py
@@ -0,0 +1,10 @@
+from re import S
+
+
+manufacturer: str = "Sukhoj"
+type: str = "SU-35S"
+releaseYear: int = 2008
+speed: float = 2.4
+unit: str = "m/s"
+
+print(f"Oroszország legmodernebb vadászrepülőgépe a { releaseYear}-ban bemutatott {manufacturer} gyártott {speed} Mach sebességre, ami 720{unit}-nak felel meg.")
