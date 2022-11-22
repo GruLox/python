af31c5749600e75a19041a4cbbea9f757668c33d
GruLox <grundmarcell@gmail.com>
2022-11-22 20:09:50 +0100

    dolgozat minta

--- 05 - Elágazások (if-else)/Feladat 08/main.py
+++ 05 - Elágazások (if-else)/Feladat 08/main.py
@@ -7,3 +7,7 @@ if(num1 % 4 == 0 and num1 % 6 == 0):
     print("A szám osztható 4-gyel és 6-tal is.")
 else:
     print("A szám nem osztható 4-gyel és 6-tal is.")
+
+import math
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
