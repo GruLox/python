ef218167f4a55803cc7644b96dac68206538c171
GruLox <grundmarcell@gmail.com>
2022-11-25 07:53:03 +0100

    2022-11-25

--- Grund Marcell/main.py
+++ Grund Marcell/main.py
@@ -1,24 +1,74 @@
-name:str= None
-brand:str= None
-model:str= None
-releaseYear:int= None
-budget:int=None
+#iskolaválasztás
+#3 féle kedvezmény, mindet kiszámolni, mindet kiíratni
+#legjobb ajánlat if statementtel
 
-print("Hogy hívják? ", end='')
-name = str(input())
+choice: int = None
+basePrice: int = None
+numberOfStudents: int = None
+groupDiscount: float = None
+schoolDiscount: float = None
+studentDiscount: float = None
+bestDeal : str = ""
+bestPrice: float = None
 
-print("Milyen márkájú járművet szeretne? ", end='')
-brand = str(input())
+print("Kérem válassza ki a helyszínt!\n1 - Velencei tó\n2 - Tihany és Balatonfüred")
+choice = int(input())
 
-print("Milyen modellt szeretne? ", end='')
-model = str(input())
+if(choice == 1):
+    basePrice = 6000
+else: 
+    basePrice = 7500
 
-print("Milyen évjáratú járművet szeretne? ", end='')
-releaseYear = str(input())
+print("Kérem a létszámot: ")
+numberOfStudents = int(input())
 
-print("Hány millió forintot szánna rá? ", end='')
-budget = str(input())
+if(numberOfStudents < 5):
+    groupDiscount = basePrice
+elif(numberOfStudents >= 5 and numberOfStudents <= 15):
+    groupDiscount = basePrice * .95
+elif(numberOfStudents >=16 and numberOfStudents <= 25):
+    groupDiscount = basePrice * .92
+else:
+    groupDiscount = basePrice * .85
 
-print(f"{name}.\nJó napot kedves {name}. Miben segíthetek?\nEngem érdekelne egy: {brand}.\nÉrtem, egy {brand}. Melyik modell?\n{model}\nÉvjárat?\n{releaseYear}\nHány milliót szánna rá?\n{budget}")
+if(numberOfStudents < 5):
+    schoolDiscount = basePrice
+elif(numberOfStudents >= 5 and numberOfStudents <= 10):
+    schoolDiscount = basePrice * (numberOfStudents - 1) / numberOfStudents
+elif(numberOfStudents >= 11 and numberOfStudents <= 20):
+    schoolDiscount = basePrice * (numberOfStudents - 2) / numberOfStudents
+else:
+    schoolDiscount = basePrice * (numberOfStudents - 3) / numberOfStudents
 
-print(f"Sajnálom kedves {name}, de jelenleg {releaseYear}-es évjáratú és {brand}s {model} nincs a kínálatunkban ami beleférne a {budget} millió forintba.")
+studentDiscount = basePrice * .92
+
+if(groupDiscount < schoolDiscount and groupDiscount < studentDiscount):
+    bestDeal = "csoportkedvezmény"
+    bestPrice = groupDiscount
+elif(schoolDiscount < groupDiscount and schoolDiscount < studentDiscount):
+    bestDeal = "intézményi kedvezmény"
+    bestPrice = schoolDiscount
+elif(studentDiscount < groupDiscount and studentDiscount < schoolDiscount):
+    bestDeal = "diákkedvezmény"
+    bestPrice = studentDiscount
+elif(schoolDiscount == groupDiscount):
+    bestDeal = "intézményi vagy csoportkedvezmény ugyanazzal az árral"
+    bestPrice = schoolDiscount
+elif(schoolDiscount == studentDiscount):
+    bestDeal = "intézményi vagy diákkedvezmény ugyanazzal az árral"
+    bestPrice = schoolDiscount
+elif(groupDiscount == studentDiscount):
+    bestDeal = "csoportkedvezmény vagy diákkedvezmény ugyanazzal az árral"
+    bestPrice = groupDiscount
+
+
+print(f"Csoportkedvezményes ár: {groupDiscount}")
+print(f"Intézményi kedvezményes ár: {schoolDiscount}")
+print(f"Diák kedvezményes ár: {studentDiscount}")
+print(f"A legjobb ajánlat a/az {bestDeal}! {numberOfStudents} fő esetén: {bestPrice} Ft.")
+
+
+
+
+
+
